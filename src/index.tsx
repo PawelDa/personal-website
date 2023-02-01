32698374b342058fbcbdad1b1b6f185e1b542f3b
PawelDa <paweldabkowski0@protonmail.com>
2023-02-01 21:02:31 +0000

    initial configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('app-root')!
-const root = createRoot(container)
-root.render(<h1>Web dev without personal website is like bold barber or overweight PT...</h1>)
+const container = document.getElementById('app-root')!;
+const root = createRoot(container);
+root.render(<h1>Some title here</h1>);
