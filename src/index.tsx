d70573db9e032e47e85d0865c2545a901961792a
PawelDa <paweldabkowski0@protonmail.com>
2023-02-03 08:42:57 +0000

    babel cofig

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,8 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app-root')!;
 const root = createRoot(container);
 root.render(<h1>Some title here</h1>);
+
+// TODO
+// 1. Check babel config
+// 2. Add jest
+// 3. Add chakra ui
